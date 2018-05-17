81dd2c67d03229ea7fd70e6395abe32218a3b2e7
Garnet Yang <gyang@192-168-1-2.tpgi.com.au>
2018-05-17 22:25:26 +1000

    add profiles schema and model

---
+++ src/models/Profiles.js
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+const STRING = Schema.Types.String;
+
+const ProfilesSchema = new Schema({
+  user: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: "users" // ref to user collection
+  },
+  handle: {
+    type: STRING,
+    required: true,
+    maxlength: 40
+  },
+  company: { type: STRING },
+  webSite: { type: STRING },
+  location: { type: STRING },
+  status: { type: STRING, required: true },
+  skills: { type: [STRING], required: true },
+  bio: { type: STRING, required: true },
+  githubusername: { type: STRING },
+  experiences: [
+    {
+      title: { type: STRING, required: true },
+      company: { type: STRING, required: true },
+      location: { type: STRING },
+      from: { type: Schema.Types.Date, required: true },
+      to: { type: Schema.Types.Date },
+      current: { type: Schema.Types.Boolean, default: false },
+      description: { type: STRING }
+    }
+  ],
+  education: [
+    {
+      school: { type: STRING, required: true },
+      degree: { type: STRING, required: true },
+      fieldOfStudy: { type: STRING, required: true },
+      from: { type: Schema.Types.Date, required: true },
+      to: { type: Schema.Types.Date },
+      current: { type: Schema.Types.Boolean, default: false },
+      description: { type: STRING }
+    }
+  ],
+  social: {
+    youtube: { type: STRING },
+    twitter: { type: STRING },
+    facebook: { type: STRING },
+    linkedIn: { type: STRING },
+    instagram: { type: STRING }
+  },
+  date: {
+    type: Schema.Types.Date,
+    default: Date.now
+  }
+});
+
+const ProfilesModel = mongoose.model("profiles", ProfilesSchema);
+
+export default ProfilesModel;
