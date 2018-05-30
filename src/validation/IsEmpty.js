9bee7adecd2360758b43e948946c6648ef317afb
gyang <gyang@stayz.com.au>
2018-05-30 14:06:47 +1000

    add support for es2017

--- src/validation/IsEmpty.js
+++ src/validation/IsEmpty.js
@@ -1,6 +1,5 @@
 const isEmpty = (obj) => {
-    return  obj === undefined ||
-    obj === null ||
+    return obj === undefined || obj === null ||
     (typeof obj === 'object' && Object.keys(obj).length === 0) ||
     (typeof obj === 'string' && obj.trim().length === 0) ||
     (Array.isArray(obj) && obj.length === 0);
