fb9a76586da406198b39789da2b9c5bad2825a84
Garnet Yang <gyang@192-168-1-2.tpgi.com.au>
2018-06-15 23:00:26 +1000

    add layout pages and configure react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
 import './App.css';
+import NavBar from './components/layout/navbar';
+import Landing from './components/layout/landidng';
+import Footer from './components/layout/footer';
+import Login from './components/auth/login';
+import Register from './components/auth/register';
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <h1>React</h1>
-            </div>
+            <BrowserRouter>
+                <div className="App">
+                    <NavBar />
+                    <Route exact path='' component={Landing} />
+                    <div className="container">
+                        <Route exact path='/login' component={Login} />
+                        <Route exact path='/register' component={Register} />
+                    </div>
+                    <Footer />
+                </div>
+            </BrowserRouter>
         )
     }
 }
