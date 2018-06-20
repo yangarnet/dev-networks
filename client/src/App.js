e82f574f68f2ea0b680a1d348b9e692937c44cd8
Garnet Yang <gyang@192-168-1-2.tpgi.com.au>
2018-06-20 15:33:42 +1000

    setup redux for react

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'; // this is where the redux store
 import './App.css';
 import NavBar from './components/layout/navbar';
 import Landing from './components/layout/landidng';
 import Footer from './components/layout/footer';
 import Login from './components/auth/login';
 import Register from './components/auth/register';
+import appStore from './store/store';
 
 class App extends Component {
     render() {
         return (
-            <BrowserRouter>
-                <div className="App">
-                    <NavBar />
-                    <Route exact path='' component={Landing} />
-                    <div className="container">
-                        <Route exact path='/login' component={Login} />
-                        <Route exact path='/register' component={Register} />
+            <Provider store={appStore}>
+                <BrowserRouter>
+                    <div className="App">
+                        <NavBar />
+                        <Route exact path='' component={Landing} />
+                        <div className="container">
+                            <Route exact path='/login' component={Login} />
+                            <Route exact path='/register' component={Register} />
+                        </div>
+                        <Footer />
                     </div>
-                    <Footer />
-                </div>
-            </BrowserRouter>
+                </BrowserRouter>
+            </Provider>
         )
     }
 }
---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,12 @@
+const initState = {
+    isAuthenticated: false,
+    user: {}
+};
+
+export const authReducer = (state = initState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
