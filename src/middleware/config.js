0a45a52c5aad0ca35cc59365cd71f466a8de1c5f
Garnet Yang <gyang@192-168-1-3.tpgi.com.au>
2018-07-31 12:41:03 +1000

    production configuration for the fullstack react node app

--- src/middleware/config.js
+++ src/middleware/config.js
@@ -1,8 +1,10 @@
-import graphqlHTTP from 'express-graphql';
-import passport from 'passport';
-import bodyParser from 'body-parser';
-import passportConfig from './passport';
-import graphqlSchema from '../graphql/schema';
+import path from "path";
+import express from "express";
+import graphqlHTTP from "express-graphql";
+import passport from "passport";
+import bodyParser from "body-parser";
+import passportConfig from "./passport";
+import graphqlSchema from "../graphql/schema";
 
 const middlewareConfig = app => {
     // body parse
@@ -14,10 +16,23 @@ const middlewareConfig = app => {
     passportConfig(passport);
 
     //mount the graphql server to /graphql
-    app.use('/graphql', graphqlHTTP({
-        schema: graphqlSchema,
-        graphiql: true
-    }));
+    app.use(
+        "/graphql",
+        graphqlHTTP({
+            schema: graphqlSchema,
+            graphiql: true
+        })
+    );
+
+    // render react
+    if (process.env.NODE_ENV === "production") {
+        app.use(express.static("client/build"));
+        app.get("*", (req, res) => {
+            res.sendFile(
+                path.resolve(__dirname, "client", "build", "index.html")
+            );
+        });
+    }
 };
 
 export default middlewareConfig;
