3e671087a590b8c8211f54aff5c8c9c84e66cbb3
Garnet Yang <gyang@192-168-1-3.tpgi.com.au>
2018-08-01 09:01:44 +1000

    some more further server configs

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 import express from "express";
+import path from "path";
 import serverConfig from "./src/config/server-config";
 
 const server = express();
 serverConfig(server);
+// react node full stack, serve react resource
+if (process.env.NODE_ENV === "production") {
+    server.use(express.static("client/build"));
+    server.get("/service-worker.js", (req, res) => {
+        res.sendFile(
+            path.resolve(__dirname, "client", "build", "service-worker.js")
+        );
+    });
+    server.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 server.listen(process.env.PORT, () => {
     console.log(`server running @ port: ${process.env.PORT}`);
--- src/routes/route-config.js
+++ src/routes/route-config.js
@@ -1,31 +1,9 @@
-import express from "express";
-import path from "path";
 import userRoute from "./api/user-route";
 import profileRoute from "./api/profile-route";
 import postRoute from "./api/post-route";
 
 // root route config begins here
 const routeConfig = app => {
-    // react node full stack, serve react resource
-    if (process.env.NODE_ENV === "production") {
-        // Set static folder
-        app.use(express.static("../../client/build"));
-        app.get("/service-worker.js", (req, res) => {
-            res.sendFile(
-                path.resolve(
-                    __dirname,
-                    "../../client",
-                    "build",
-                    "service-worker.js"
-                )
-            );
-        });
-        app.get("*", (req, res) => {
-            res.sendFile(
-                path.resolve(__dirname, "../../client", "build", "index.html")
-            );
-        });
-    }
     app.use("/api/user", userRoute());
     app.use("/api/profile", profileRoute());
     app.use("/api/post", postRoute());
