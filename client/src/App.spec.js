c9cbe139a453fc93330d0d104246be2065ef0b8e
Garnet Yang <gyang@192-168-1-3.tpgi.com.au>
2018-08-01 16:12:13 +1000

    add nodemon for react test watch

--- client/src/App.spec.js
+++ client/src/App.spec.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {expect, assert} from 'chai';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { expect, assert } from "chai";
+import App from "./App";
 
-
-describe('test cases for APP', ()=>{
-    it('the APP should be rendered without crashing', ()=>{
-        const div = document.createElement('section');
-        ReactDOM.render(<App/>, div);
+describe("test cases for APP", () => {
+    it("the APP should be rendered without crashing", () => {
+        const div = document.createElement("section");
+        ReactDOM.render(<App />, div);
+        expect(div.innerHTML).to.contain("Sign Up");
+        expect(div.innerHTML).to.contain("Login");
         ReactDOM.unmountComponentAtNode(div);
     });
 });
