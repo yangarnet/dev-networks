13420b847baa62edb8b9231990233797cd6c1e11
Garnet Yang <gyang@192-168-1-3.tpgi.com.au>
2018-08-08 09:24:27 +1000

    add unit test for text area field group

--- client/src/components/common/test/InputGroup.spec.js
+++ client/src/components/common/test/InputGroup.spec.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { mount } from "enzyme";
-import sinon from "sinon";
 import InputGroup from "../InputGroup";
 
 let wrapped;
---
+++ client/src/components/common/test/TextAreaFieldGroup.spec.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { mount } from "enzyme";
+import TextAreaFieldGroup from "../TextAreaFieldGroup";
+import { wrap } from "module";
+
+let wrapped;
+
+describe("TextAreaFieldGroup unit test", () => {
+    beforeEach(() => {
+        const props = {
+            name: "comment",
+            placeholder: "input your comment here",
+            onChange: () => {},
+            errors: {}
+        };
+
+        wrapped = mount(<TextAreaFieldGroup {...props} />);
+    });
+
+    afterEach(() => {
+        wrapped.unmount();
+    });
+
+    it("should render TextAreaFieldGroup", () => {
+        expect(wrapped.find("div.form-group")).to.have.lengthOf(1);
+        expect(wrapped.find("textarea")).to.have.lengthOf(1);
+        const props = wrapped.find("textarea").props();
+        expect(props.name).to.be.equal("comment");
+        expect(props.onChange).to.be.a("function");
+        expect(props.placeholder).to.be.equal("input your comment here");
+
+        expect(wrapped.props().errors).to.be.an("object");
+        expect(wrapped.props().errors).to.be.empty;
+    });
+
+    it("should displace error message", () => {
+        // set the props with new value
+        wrapped.setProps({
+            errors: { email: "your input email is invalid" },
+            name: "email"
+        });
+
+        wrapped.update();
+
+        expect(wrapped.props().errors).to.be.an("object");
+        expect(wrapped.props().errors).to.be.not.empty;
+        expect(wrapped.props().errors.email).to.be.equal(
+            "your input email is invalid"
+        );
+        expect(wrapped.find("div.invalid-feedback")).to.have.lengthOf(1);
+        expect(wrapped.find("div.invalid-feedback").text()).to.be.equal(
+            "your input email is invalid"
+        );
+    });
+});
